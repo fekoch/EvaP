4c685a08210743c36ec2ad2b24897c1fb0954d00
Felix Koch <felixkoch.it@gmail.com>
2025-12-15 22:43:02 +0100

    format

--- evap/evaluation/tests/tools.py
+++ evap/evaluation/tests/tools.py
@@ -364,6 +364,7 @@ class LiveServerTest(SeleniumTestCase):
         super().setUpClass()
         cls.selenium.set_window_size(*cls.window_size)
 
+
 def untag(*tags_to_remove):
     """Decorator to remove tags from a test class or method."""
 
@@ -373,6 +374,7 @@ def untag(*tags_to_remove):
         return obj
 
     return decorator
+
 
 @override_settings(SLOGANS_EN=["Einigermaßen verlässlich aussehende Pixeltestung"])
 class VisualRegressionTestCase(LiveServerTest):
